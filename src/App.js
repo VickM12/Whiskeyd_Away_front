604ae9aaa07973e472d3a7363972a06d00c4854d
Vicktor M <vicktor.moberg@gmail.com>
2020-09-24 10:58:27 -0400

    Added form to submit a new whiskey

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ import './App.css';
 function App() {
   
   const [whiskeys, setWhiskeys] = useState([])
+  const [formInputs, updateFormInputs] = useState({
+    name: '',
+    distiller: '',
+    origin:'',
+    img:''
+  })
+  const handleChange = (event) =>{
+    const updateInput = Object.assign({}, formInputs, { [event.target.id]: event.target.value})
+    updateFormInputs(updateInput)
+  }
+  const handleSubmit = (event) =>{
+    event.preventDefault()
+    console.log(formInputs)
+  }
   
   const getData = async() => {
   try {
@@ -16,14 +30,31 @@ function App() {
     console.log(error)
   } 
 }
-useEffect(() => {
-  (async function () {
-      await getData();
-  })();
-}, []);
+  useEffect(() => {
+    (async function () {
+        await getData();
+    })();
+    }, []);
+
   return (
     <div className="App">
-     
+     <nav>
+       <h4>Submit a whiskey!</h4>
+       <form onSubmit={handleSubmit}>
+         <label htmlFor="name">Name</label>
+         <input type='text' id='name' value={formInputs.name}
+         onChange={handleChange}/>
+         <label htmlFor='distiller'>Distillery</label>
+         <input type='text' id='distiller' value={formInputs.distiller}
+         onChange={handleChange} />
+         <label htmLFor="origin">Origin</label>
+         <input type='text' id='origin' value={formInputs.origin}
+         onChange={handleChange}/>
+         <label htmlFor='img'>Image</label>
+         <input type='text' id='img' value={formInputs.img}
+         onChange={handleChange}/>
+       </form>
+     </nav>
       <main>
         <Whiskeys whiskeyData = {whiskeys}/>
       </main>
